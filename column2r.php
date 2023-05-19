1547de4c5d5689ae8c83baa183c498e2b7199f9e
ilyeach <ilyeach121212@gmail.com>
2023-05-19 13:21:14 +0530

    change column2r page

--- column2r.php
+++ column2r.php
@@ -20,7 +20,10 @@
   <div class="row">
   
   <div class="col-sm-8">
-      <h2>Lorem Ipsum</h2>
+      <h2>Lorem-Ipsum</h2>
+	  
+	  
+	  
       <h5>Why do we use it?</h5>
       <p><small>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</small></p>
       <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
